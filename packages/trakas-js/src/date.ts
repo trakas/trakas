91fb50c8771512c2665e56fb84ec948a12e6a0a1
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2021-03-29 07:48:35 +0700

    perf(js): optimize formatTimeInSecond when converting time in second

--- packages/trakas-js/src/date.ts
+++ packages/trakas-js/src/date.ts
@@ -3,9 +3,7 @@ function padNumber(number: number): string {
 }
 
 export function formatTimeInSecond(second: string | number): string {
-  const time = Number(
-    typeof second === "string" ? parseFloat(second).toFixed(0) : second.toFixed(0)
-  );
+  const time = Math.floor(Number(second));
   const hours = Math.floor(time / 60 ** 2);
   const minutes = Math.floor(time / 60) % 60;
   const seconds = Math.floor(time - minutes * 60 - hours * 60 ** 2);
