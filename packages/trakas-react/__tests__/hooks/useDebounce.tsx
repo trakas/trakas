8cb5fbf6d4e4dde34fec0206687447b3ebcb1918
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2021-04-01 13:16:59 +0700

    refactor(js): rework on `useDebounce` tests

--- packages/trakas-react/__tests__/hooks/useDebounce.tsx
+++ packages/trakas-react/__tests__/hooks/useDebounce.tsx
@@ -1,38 +1,55 @@
 import { act, render, screen } from "@testing-library/react";
-import { useEffect, useState } from "react";
+import userEvent from "@testing-library/user-event";
+import { useState } from "react";
 import { useDebounce } from "../../src";
 
 function TestComponent() {
-  const [value, setValue] = useState("1");
-  const debounceValue = useDebounce(value, 3000);
+  const [value, setValue] = useState(1);
+  const debounceValue = useDebounce(value, 5000);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setValue("2");
-    }, 2000);
-  }, []);
+  const handleClick = () => {
+    setValue((value) => value + 1);
+  };
 
-  return <div>{debounceValue}</div>;
+  return <button onClick={handleClick}>{debounceValue}</button>;
 }
 
 describe("useDebounce", () => {
   test("not updates value within 5s", async () => {
     render(<TestComponent />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
     act(() => {
       jest.advanceTimersByTime(4000);
     });
 
-    const textElement = screen.queryByText("2");
-    expect(textElement).toBeNull();
+    expect(buttonElement).toHaveTextContent("1");
   });
 
   test("updates value after 5s", async () => {
     render(<TestComponent />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
     act(() => {
-      jest.advanceTimersByTime(5001);
+      jest.advanceTimersByTime(5000);
     });
 
-    const textElement = screen.queryByText("2");
-    expect(textElement).not.toBeNull();
+    expect(buttonElement).toHaveTextContent("2");
+  });
+
+  test("has no memory leak when a new change occurs by the delay time moment", async () => {
+    render(<TestComponent />);
+
+    const buttonElement = screen.getByRole("button");
+    act(() => {
+      userEvent.click(buttonElement);
+      jest.advanceTimersByTime(4000);
+      userEvent.click(buttonElement);
+      jest.advanceTimersByTime(2000);
+    });
+
+    expect(buttonElement).toHaveTextContent("1");
   });
 });
